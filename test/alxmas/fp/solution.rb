1054485cb4ab2e636f700b3d7d424b45a78cf48c
alXmas <alexsandr.xmas@gmail.com>
2019-01-24 14:45:12 +0300

    fp tests were passed successfully

--- test/alxmas/fp/solution.rb
+++ test/alxmas/fp/solution.rb
@@ -4,13 +4,20 @@ module Alxmas
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        films = array.select do |film|
+          film['rating_kinopoisk'].to_f.positive? &&
+              film['country'].to_s.split(',').count >= 2
+        end
+        rating = films.reduce(0) {|acc, film| acc + film['rating_kinopoisk'].to_f}
+        rating/films.count
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        films.select { |film| film['rating_kinopoisk'].to_f >= threshold &&
+            film['name'].include?('и') }
+            .count
+        end
       end
     end
   end
-end
--- test/alxmas/fp/test.rb
+++ test/alxmas/fp/test.rb
@@ -6,22 +6,20 @@ class Alxmas::FpTest < Minitest::Test
   # Посчитать средний рейтинг фильмов по версии кинопоиска у которых две или больше стран
   # Фильмы у которых рейтиг не задан или равен 0 не учитывать в расчете среднего.
   def test_rating
-    skip
     array = CSV.readlines('./test/fixtures/films.csv', headers: true)
 
     result = Alxmas::Fp.rating(array)
     assert result == 6.809410385259628
   end
 
-  # Посчитать количесвто букв 'и' в названиях всех фильмов с рейтингом кинопоиска больше или равным заданному значению
+  # Посчитать количесвто фильмов с буквой 'и' в названии и рейтингом кинопоиска больше или равным заданному значению
   def test_chars_count
-    skip
     array = CSV.readlines('./test/fixtures/films.csv', headers: true)
 
     result = Alxmas::Fp.chars_count(array, 5)
-    assert result == 891
+    assert result == 1891
 
     result = Alxmas::Fp.chars_count(array, 8.5)
-    assert result == 1
+    assert result == 20
   end
 end
